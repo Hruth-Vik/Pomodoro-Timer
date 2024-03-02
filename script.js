1267c1744036d720c0f0351e4d43ef0b9ebd20a4
HruthVik <hruthviknaik03@gmail.com>
2024-03-02 22:30:07 +0530

    edited js file

--- script.js
+++ script.js
@@ -3,6 +3,19 @@ let time=startingMinutes*60;
 
 const coundownEL=document.querySelector("#inital_time");
 
+setInterval(updateCountdown,1000);
+
+
 function updateCountdown(){
+    const minutes=Math.floor(time/60);
+    let seconds=time%60;
+    countdownEl.innerHtml=`${minutes}:${seconds}`;
     time--;
 }
+
+
+
+
+
+
+

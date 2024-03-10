1ae49f5de019345976611b667c3f7e36d25b9657
HruthVik <hruthviknaik03@gmail.com>
2024-03-10 23:27:24 +0530

    edited the files and trying to approach newly

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-const startingMinutes=25;
-let time=startingMinutes*60;
-
-const coundownEL=document.querySelector("#inital_time");
-
-setInterval(updateCountdown,1000);
-
-
-function updateCountdown(){
-    const minutes=Math.floor(time/60);
-    let seconds=time%60;
-    countdownEl.innerHtml=`${minutes}:${seconds}`;
-    time--;
-}
-
-
 
 
 
